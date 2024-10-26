7c2fa76e8d24013913bc15e1b81a88d6d7b17b80
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2024-10-26 21:38:27 +0330

    feat: implement UserModel with pre save validations

---
+++ models/userModel.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import validations from "@/utils/validations";
+
+interface IUser {
+  email: string;
+  name: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: [true, "email is required !"],
+    },
+    name: { type: String, default: "Dear User !" },
+    password: {
+      type: String,
+      required: [true, "password is required !"],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!validations.isEmail(this.email)) {
+    next(new Error("email is invalid !"));
+  }
+  if (!validations.isPassword(this.password)) {
+    next(
+      new Error(
+        "password must be contains at least 8 chars, small and capital letters and can not be persian !"
+      )
+    );
+  }
+  const hashedPassword = await bcrypt.hash(this.password, 12);
+  this.password = hashedPassword;
+  next();
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
